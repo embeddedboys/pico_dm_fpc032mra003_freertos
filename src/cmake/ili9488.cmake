22d81d792bfed11a1a7e2c7aa2093a6c11b54779
iotah <writeforever@foxmail.com>
2024-04-05 13:16:16 +0800

    add ili9488 cmake info.
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ src/cmake/ili9488.cmake
@@ -0,0 +1,13 @@
+set(LCD_PIN_DB_BASE  0)  # 8080 LCD data bus base pin
+set(LCD_PIN_DB_COUNT 16) # 8080 LCD data bus pin count
+set(LCD_PIN_CS  29)  # 8080 LCD chip select pin
+set(LCD_PIN_WR  19)  # 8080 LCD write pin
+set(LCD_PIN_RS  20)  # 8080 LCD register select pin
+set(LCD_PIN_RD  18)  # 8080 LCD read pin
+set(LCD_PIN_RST 22)  # 8080 LCD reset pin
+set(LCD_PIN_BL  28)  # 8080 LCD backlight pin
+set(LCD_HOR_RES 480)
+set(LCD_VER_RES 320)
+set(DISP_OVER_PIO 1) # 1: PIO, 0: GPIO
+set(PIO_USE_DMA   1)   # 1: use DMA, 0: not use DMA
+set(I80_BUS_WR_CLK_KHZ 50000)
